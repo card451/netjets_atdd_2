c8c0b9e1a089dc88f6d855f0a4820cf8126cb5bc
card451 <rcard@netjets.com>
2015-05-12 17:11:29 -0400

    updated SW lab

--- Labs/southwest/features/pages/southwest_home.rb
+++ Labs/southwest/features/pages/southwest_home.rb
@@ -8,5 +8,5 @@ page_url('http://www.southwest.com')
 text_field(:departure_airport, :id => 'air-city-departure')
 text_field(:arrival_airport, :id => 'air-city-arrival')
 button(:search, :id => 'jb-booking-form-submit-button')
-text_field(:error_msg, :div => 'Enter a destination airport' )
+div(:error_msg, :class => 'js-destination-airport-label booking-form--label booking-form--location-container-label error-label')
 end
