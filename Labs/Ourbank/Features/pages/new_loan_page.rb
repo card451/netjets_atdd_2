82b23f2c7bcb5039bf074516ea4fe335b3e60b2f
card451 <rcard@netjets.com>
2015-05-14 09:40:50 -0400

    fixed

--- Labs/Ourbank/Features/pages/new_loan_page.rb
+++ Labs/Ourbank/Features/pages/new_loan_page.rb
@@ -1,3 +1,10 @@
+require 'page-object'
+require 'watir-webdriver'
+
 def goto_new_loan_page
+  include PageObject
  # browser.goto ('http://www.google.com')
+  puts 'True'
+  page_url('http://www.google.com')
+
 end
--- Labs/Ourbank/Features/step_definitions/new_loan_steps.rb
+++ Labs/Ourbank/Features/step_definitions/new_loan_steps.rb
@@ -5,35 +5,39 @@ include PageObject::PageFactory
 
 
 When(/^Consumer submits valid new loan application$/) do
-  goto_new_loan_page
+#todo
+#  goto_new_loan_page
   #pending
   #Valid Name
   #Valid Address
   #Loan amount > 0 and < MAX
 
-
-end
+ end
 
 Then(/^Confirmation message displayed$/) do
-  goto_new_loan_page
+  #todo
+  #goto_new_loan_page
   #pending
   #Confirmation message displays
 
 
-end
+ end
 
 When(/^Consumer submits a new loan application with (.*)$/) do |invalid_criteria|
-  #pending
+  #todo
+
+  #goto_new_loan_page
 def invalid_criteria
   #Invalid Criteria
   #Blank Name
   #Blank Address
   #Loan Amount <= 0
   #Loan Amount >= MAX
-end
+ end
 end
 
 Then(/^Invalid criteria error message is displayed$/) do
+  #todo
   #pending
 #Invalid Criteria error message displays
 end
